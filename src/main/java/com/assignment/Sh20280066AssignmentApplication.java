8586919e831b5ac05839464cbeec31e8648321f9
shailendra_vishwakarma <shailendracse1998@hgmail.com>
2022-11-04 14:06:52 +0530

    first commit

---
+++ src/main/java/com/assignment/Sh20280066AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sh20280066AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sh20280066AssignmentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/assignment/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.assignment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	@RequestMapping("/test")
+	public String m1()
+	{
+		return "This is only for test purpose";
+	}
+	@RequestMapping("/test2")
+	public String m2()
+	{
+		return "This is another test for different RESTFull service";
+	}
+
+}
